5c0025755eadac02c075a1dcad7ee1bbf4d6796f
roy897 <roushangupta85@gmail.com>
2022-05-17 22:49:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Os } from './components/os';
+
+
+function App() {
+  return (
+    <Os/>     
+  );
+}
+
+export default App;
---
+++ src/components/os.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+let Os = () => {  
+  
+    return <div>
+        <h1>Mobile Operating System</h1>
+        <ul>           
+            <li>Android</li>
+            <li>Bleckberry</li>
+            <li>Windows Phone</li>
+            <li>Windows Phone</li>
+        </ul>
+        <h1>Mobile Manufacturers</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+}
+
+export {Os};
+
+
+
